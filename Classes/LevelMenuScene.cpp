6fb50d71c87e54467da11dac7f5fadd06e1af9b5
Kintra <arttumarttinen@gmail.com>
2015-07-21 15:54:42 +0300

    Graphically improved level select menu, centered buttons and more dynamic positioning

--- Classes/LevelMenuScene.cpp
+++ Classes/LevelMenuScene.cpp
@@ -50,7 +50,7 @@ bool LevelMenuScene::init()
 		playButton1 = MenuItemImage::create("Level_1_wood.png", "Level_1_wood.png", CC_CALLBACK_1(
 			LevelMenuScene::GoToGameScene, this));
 		playButton1->setName("Maps/First.tmx");
-		auto label1 = Label::create("", "fonts/Marker Felt.ttf", 32);
+		auto label1 = Label::create("", "fonts/Marker Felt.ttf", 28);
 
 		float _highscore = def->getIntegerForKey(playButton1->getName().c_str(), 0);
 		std::string shortName = playButton1->getName().erase(0, 5);
@@ -59,15 +59,15 @@ bool LevelMenuScene::init()
 		String *text1 = String::createWithFormat("Hiscore: %.2f", _highscore);
 		label1->setString(text1->getCString());
 
-		playButton1->setPosition(Point(0, visibleSize.height));
+		playButton1->setPosition(Point((visibleSize.width - playButton1->getContentSize().width * 4) / 2, visibleSize.height));
 		playButton1->setAnchorPoint(Vec2(0, 1));
-		label1->setPosition(Vec2(playButton1->getPosition().x + 210 / 2, playButton1->getPosition().y - 190));
+		label1->setPosition(Vec2(playButton1->getPosition().x + playButton1->getContentSize().width / 2, playButton1->getPosition().y - 190));
 
 	//Level 2 button
 		playButton2 = MenuItemImage::create("Level_2_wood.png", "Level_2_wood.png", CC_CALLBACK_1(
 			LevelMenuScene::GoToGameScene, this));
 		playButton2->setName("Maps/Second.tmx");
-		auto label2 = Label::create(playButton2->getName().c_str(), "fonts/Marker Felt.ttf", 32);
+		auto label2 = Label::create(playButton2->getName().c_str(), "fonts/Marker Felt.ttf", 28);
 
 		float _highscore2 = def->getIntegerForKey(playButton2->getName().c_str(), 0);
 		std::string shortName2 = playButton2->getName().erase(0, 5);
@@ -78,13 +78,13 @@ bool LevelMenuScene::init()
 	
 		playButton2->setPosition(Vec2(playButton1->getPosition().x + playButton1->getContentSize().width, playButton1->getPosition().y));
 		playButton2->setAnchorPoint(Vec2(0, 1));
-		label2->setPosition(Vec2(playButton2->getPosition().x + 105, playButton2->getPosition().y - 190));
+		label2->setPosition(Vec2(playButton2->getPosition().x + playButton2->getContentSize().width / 2, playButton2->getPosition().y - 190));
 
 	//Level 3 button
 		playButton3 = MenuItemImage::create("Level_3_wood.png", "Level_3_wood.png", CC_CALLBACK_1(
 			LevelMenuScene::GoToGameScene, this));
 		playButton3->setName("Maps/Third.tmx");
-		auto label3 = Label::create(playButton3->getName().c_str(), "fonts/Marker Felt.ttf", 32);
+		auto label3 = Label::create(playButton3->getName().c_str(), "fonts/Marker Felt.ttf", 28);
 
 		float _highscore3 = def->getIntegerForKey(playButton3->getName().c_str(), 0);
 		std::string shortName3 = playButton3->getName().erase(0, 5);
@@ -95,7 +95,7 @@ bool LevelMenuScene::init()
 
 		playButton3->setPosition(Vec2(playButton2->getPosition().x + playButton3->getContentSize().width, playButton2->getPosition().y));
 		playButton3->setAnchorPoint(Vec2(0, 1));
-		label3->setPosition(Vec2(playButton3->getPosition().x + 105, playButton3->getPosition().y - 190));
+		label3->setPosition(Vec2(playButton3->getPosition().x + playButton3->getContentSize().width / 2, playButton3->getPosition().y - 190));
 
 	//Level 4 button
 		playButton4 = MenuItemImage::create("Level_4_wood.png", "Level_4_wood.png", CC_CALLBACK_1(
@@ -116,90 +116,90 @@ bool LevelMenuScene::init()
 		label4->setPosition(Vec2(playButton4->getPosition().x + playButton4->getContentSize().width / 2, playButton4->getPosition().y - 190));
 
 	//Level 5 button
-		playButton5 = MenuItemImage::create("levelBG.png", "levelBG.png", CC_CALLBACK_1(
+		playButton5 = MenuItemImage::create("Level_5_wood.png", "Level_5_wood.png", CC_CALLBACK_1(
 			LevelMenuScene::GoToGameScene, this));
 		playButton5->setName("Maps/Fifth.tmx");
-		auto label5 = Label::create(playButton5->getName().c_str(), "fonts/Marker Felt.ttf", 32);
+		auto label5 = Label::create(playButton5->getName().c_str(), "fonts/Marker Felt.ttf", 28);
 
 		float _highscore5 = def->getIntegerForKey(playButton5->getName().c_str(), 0);
 		std::string shortName5 = playButton5->getName().erase(0, 5);
 		shortName5.erase(shortName5.length() - 4, 4);
 
-		String *text5 = String::createWithFormat("%s: \n%.2f", shortName5.c_str(), _highscore5);
+		String *text5 = String::createWithFormat("Hiscore: %.2f", _highscore5);
 		label5->setString(text5->getCString());
 
 
-		playButton5->setPosition(Vec2(playButton1->getPosition().x, playButton1->getContentSize().height));
+		playButton5->setPosition(Vec2(playButton1->getPosition().x, visibleSize.height - playButton1->getContentSize().height));
 		playButton5->setAnchorPoint(Vec2(0, 1));
-		label5->setPosition(Vec2(playButton5->getPosition().x + label5->getContentSize().width / 2, playButton5->getPosition().y - label5->getContentSize().height / 2));
+		label5->setPosition(Vec2(playButton5->getPosition().x + playButton5->getContentSize().width / 2, playButton5->getPosition().y - 190));
 
 	//Level 6 button
-		playButton6 = MenuItemImage::create("levelBG.png", "levelBG.png", CC_CALLBACK_1(
+		playButton6 = MenuItemImage::create("Level_6_wood.png", "Level_6_wood.png", CC_CALLBACK_1(
 			LevelMenuScene::GoToGameScene, this));
 		playButton6->setName("Maps/Sixth.tmx");
-		auto label6 = Label::create(playButton6->getName().c_str(), "fonts/Marker Felt.ttf", 32);
+		auto label6 = Label::create(playButton6->getName().c_str(), "fonts/Marker Felt.ttf", 28);
 
 		float _highscore6 = def->getIntegerForKey(playButton6->getName().c_str(), 0);
 		std::string shortName6 = playButton6->getName().erase(0, 5);
 		shortName6.erase(shortName6.length() - 4, 4);
 
-		String *text6 = String::createWithFormat("%s: \n%.2f", shortName6.c_str(), _highscore6);
+		String *text6 = String::createWithFormat("Hiscore: %.2f", _highscore6);
 		label6->setString(text6->getCString());
 
 		playButton6->setPosition(Vec2(playButton5->getPosition().x + playButton6->getContentSize().width, playButton5->getPosition().y));
 		playButton6->setAnchorPoint(Vec2(0, 1));
-		label6->setPosition(Vec2(playButton6->getPosition().x + label6->getContentSize().width / 2, playButton6->getPosition().y - label6->getContentSize().height / 2));
+		label6->setPosition(Vec2(playButton6->getPosition().x + playButton6->getContentSize().width / 2, playButton6->getPosition().y - 190));
 
 	//Level 7 button
-		playButton7 = MenuItemImage::create("levelBG.png", "levelBG.png", CC_CALLBACK_1(
+		playButton7 = MenuItemImage::create("Level_7_wood.png", "Level_7_wood.png", CC_CALLBACK_1(
 			LevelMenuScene::GoToGameScene, this));
 		playButton7->setName("Maps/Seventh.tmx");
-		auto label7 = Label::create(playButton7->getName().c_str(), "fonts/Marker Felt.ttf", 32);
+		auto label7 = Label::create(playButton7->getName().c_str(), "fonts/Marker Felt.ttf", 28);
 
 		float _highscore7 = def->getIntegerForKey(playButton7->getName().c_str(), 0);
 		std::string shortName7 = playButton7->getName().erase(0, 5);
 		shortName7.erase(shortName7.length() - 4, 4);
 
-		String *text7 = String::createWithFormat("%s: \n%.2f", shortName7.c_str(), _highscore7);
+		String *text7 = String::createWithFormat("Hiscore: %.2f", _highscore7);
 		label7->setString(text7->getCString());
 
 		playButton7->setPosition(Vec2(playButton6->getPosition().x + playButton7->getContentSize().width, playButton6->getPosition().y));
 		playButton7->setAnchorPoint(Vec2(0, 1));
-		label7->setPosition(Vec2(playButton7->getPosition().x + label7->getContentSize().width / 2, playButton7->getPosition().y - label7->getContentSize().height / 2));
+		label7->setPosition(Vec2(playButton7->getPosition().x + playButton7->getContentSize().width / 2, playButton7->getPosition().y - 190));
 
 	//Level 8 button
-		playButton8 = MenuItemImage::create("levelBG.png", "levelBG.png", CC_CALLBACK_1(
+		playButton8 = MenuItemImage::create("Level_8_wood.png", "Level_8_wood.png", CC_CALLBACK_1(
 			LevelMenuScene::GoToGameScene, this));
 		playButton8->setName("Maps/Eight.tmx");
-		auto label8 = Label::create(playButton8->getName().c_str(), "fonts/Marker Felt.ttf", 32);
+		auto label8 = Label::create(playButton8->getName().c_str(), "fonts/Marker Felt.ttf", 28);
 
 		float _highscore8 = def->getIntegerForKey(playButton8->getName().c_str(), 0);
 		std::string shortName8 = playButton8->getName().erase(0, 5);
 		shortName8.erase(shortName8.length() - 4, 4);
 
-		String *text8 = String::createWithFormat("%s: \n%.2f", shortName8.c_str(), _highscore8);
+		String *text8 = String::createWithFormat("Hiscore: %.2f", _highscore8);
 		label8->setString(text8->getCString());
 
 		playButton8->setPosition(Vec2(playButton7->getPosition().x + playButton8->getContentSize().width, playButton7->getPosition().y));
 		playButton8->setAnchorPoint(Vec2(0, 1));
-		label8->setPosition(Vec2(playButton8->getPosition().x + label8->getContentSize().width / 2, playButton8->getPosition().y - label8->getContentSize().height / 2));
+		label8->setPosition(Vec2(playButton8->getPosition().x + playButton8->getContentSize().width / 2, playButton8->getPosition().y - 190));
 
 	//Level 9 button
-		playButton9 = MenuItemImage::create("levelBG.png", "levelBG.png", CC_CALLBACK_1(
+		playButton9 = MenuItemImage::create("Level_9_wood.png", "Level_9_wood.png", CC_CALLBACK_1(
 			LevelMenuScene::GoToGameScene, this));
 		playButton9->setName("Maps/Ninth.tmx");
-		auto label9 = Label::create(playButton9->getName().c_str(), "fonts/Marker Felt.ttf", 32);
+		auto label9 = Label::create(playButton9->getName().c_str(), "fonts/Marker Felt.ttf", 28);
 
 		float _highscore9 = def->getIntegerForKey(playButton9->getName().c_str(), 0);
 		std::string shortName9 = playButton9->getName().erase(0, 5);
 		shortName9.erase(shortName9.length() - 4, 4);
 
-		String *text9 = String::createWithFormat("%s: \n%.2f", shortName9.c_str(), _highscore9);
+		String *text9 = String::createWithFormat("Hiscore: %.2f", _highscore9);
 		label9->setString(text9->getCString());
 
-		playButton9->setPosition(Vec2(playButton8->getPosition().x + playButton9->getContentSize().width, playButton8->getPosition().y));
+		playButton9->setPosition(Vec2(visibleSize.width / 2 - playButton9->getContentSize().width / 2, playButton5->getPosition().y - playButton9->getContentSize().height));
 		playButton9->setAnchorPoint(Vec2(0, 1));
-		label9->setPosition(Vec2(playButton9->getPosition().x + label9->getContentSize().width / 2, playButton9->getPosition().y - label9->getContentSize().height / 2));
+		label9->setPosition(Vec2(playButton9->getPosition().x + playButton9->getContentSize().width / 2, playButton9->getPosition().y - 190));
 
 	// Confirm
 	confirmButton = MenuItemImage::create("CloseNormal.png", "CloseNormal.png", CC_CALLBACK_1(
